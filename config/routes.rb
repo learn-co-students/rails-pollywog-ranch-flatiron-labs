b8e9955dec5bca692d2c62df072879f000889552
Marco Chin <marcoc@umich.edu>
2018-06-28 16:10:39 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
-Rails.application.routes.draw do
 
-  # root
-  root to: 'static_pages#home'
-
-  # destroy as get requests
-  get '/ponds/:id/destroy', to: 'ponds#destroy'
-  get '/frogs/:id/destroy', to: 'frogs#destroy'
-  get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
-
-  # code your post to  '/tadpoles/:id/metamorphose' here
-  # it should direct to the tadpoles controller, the metamorphose action
-
-  # resources
-  resources :ponds
 
   resources :tadpoles, :only => [:index, :show, :edit, :update, :destroy, :metamorphose, :create]
 
